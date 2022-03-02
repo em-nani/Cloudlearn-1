f4dcb59c536d8c19d0c12b0b7d2123c75897e924
michelle <michelle@truefirestudios.com>
2022-03-02 12:13:00 -0500

    initial commit

---
+++ pages/learnMore.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1 class="statement">Learn more about our widgets</h1>
+    <p class="bodytext">We grow the finest widgets with our secret recipe.</p>
+    <p class="bodytext">
+        This is an App for Widgets. Coming out with new creative User Experience and user Interface
+        for user engagement! 
+        More updates coming soon...
+    </p>
+    <nuxt-link to="/" class="callToAction">Go back</nuxt-link>
+  </div>
+</template>
+
+<script>
+    export default {
+        head () {
+            return {
+                title: `About our widgets`,
+                meta: [
+                    { hid: 'description', name: 'description', content: 'WidgetCo provides the finest in modern widgets, find out how...' },
+                    { name: 'keywords', content: 'widgetCo, widgets, widget design'},
+                ]
+            }
+        }
+    }
+</script>
+
+<style>
+</style>
