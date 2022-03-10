cd1190199794dda479b3c8db3ce0397cabade7e4
michelle <michelle@truefirestudios.com>
2022-03-10 11:10:01 -0500

    Initial commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,42 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "nuxt214b03c6": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "hosting": {
+        "ElasticContainer": {
+            "ServiceName": "string",
+            "ClusterName": "string",
+            "PipelineName": "string",
+            "ContainerNames": "string",
+            "PipelineUrl": "string",
+            "LoadBalancerAliasDomainName": "string",
+            "LoadBalancerCnameDomainName": "string",
+            "CloudfrontDistributionAliasDomainName": "string",
+            "CloudfrontDistributionCnameDomainName": "string"
+        }
+    },
+    "api": {
+        "containerc1766edf": {
+            "ServiceName": "string",
+            "ClusterName": "string",
+            "PipelineName": "string",
+            "ContainerNames": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        },
+        "test": {
+            "ServiceName": "string",
+            "ClusterName": "string",
+            "PipelineName": "string",
+            "ContainerNames": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
